71235c798dfce4984bbf5f2b73ebe66836ce7b0d
Firedreyke <avogt8@student.cccs.edu>
2023-08-31 18:21:50 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// CIS 359 Assignment #1
+// Hello World!
+// by Drey :)
+
+using System;
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
